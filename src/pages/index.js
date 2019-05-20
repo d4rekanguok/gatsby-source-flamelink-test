3ad33a13bb572721463d67e0ca8c781647327971
Derek Nguyen <derek@penandpillow.com>
2019-05-20 23:13:13 +0900

    get readme going + actually use the returned image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,49 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => {
+  const posts = data.posts.nodes
+  return (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Posts</h1>
+    <hr />
+    { posts.map(({title, description, hero, flamelink__meta_ : { createdDate } }) => (
+      <section>
+        <h2>{title}</h2>
+        <p><small>{createdDate}</small></p>
+        <p>{description}</p>
+        {hero.length > 0 && <Img fixed={hero[0].localFile.childImageSharp.fixed} />}
+      </section>
+    )) }
   </Layout>
-)
+)}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    posts: allFlamelinkBlogContent {
+      nodes {
+        title
+        description
+        flamelink__meta_ {
+          createdDate(formatString: "YYYY-MM-DD")
+        }
+        hero {
+          localFile {
+            childImageSharp {
+              fixed(width: 600) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
